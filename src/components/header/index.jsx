b1275f9b3d9c49f45463e34ea7c8ccb63ed72378
Cicilini <guilherme.cicilini@hotmail.com>
2023-12-07 22:36:19 -0300

    project rick and morty api

---
+++ src/components/header/index.jsx
@@ -0,0 +1,20 @@
+import { Container } from "../style";
+import Logo from "../../assets/logo.png"
+
+export const Header = () => {
+    return ( 
+        <Container>
+        <img src={Logo} alt="logo"/>
+
+
+        <nav>
+            <a href="#">Docs</a>
+            <a href="#">About</a>
+            <a href="#">Suppor</a>
+
+        </nav>
+
+        </Container>
+    )
+
+}
